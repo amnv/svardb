77ae199cb11004b5a7f7d0407373633a1140a03b
allyson <allyson.venceslau@gmail.com>
2023-05-11 21:42:02 -0300

    Adding register phone screan and updating android and gradle version

---
+++ app/src/main/java/br/pucminas/svardb/ListSensorsActivity.java
@@ -0,0 +1,14 @@
+package br.pucminas.svardb;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListSensorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_sensors);
+    }
+}
--- app/src/main/java/br/pucminas/svardb/MainActivity.java
+++ app/src/main/java/br/pucminas/svardb/MainActivity.java
@@ -1,7 +1,10 @@
 package br.pucminas.svardb;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_list_sensors).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListSensorsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_register_phone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterPhoneActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
