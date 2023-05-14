c7887e3fc48cc5ff61846386da4dba2a00aeb0a6
allyson <allyson.venceslau@gmail.com>
2023-05-14 14:47:50 -0300

    Adding formating text and alert saving phone number

--- app/src/main/java/br/pucminas/svardb/activity/RegisterPhoneActivity.java
+++ app/src/main/java/br/pucminas/svardb/activity/RegisterPhoneActivity.java
@@ -1,9 +1,15 @@
 package br.pucminas.svardb.activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import br.pucminas.svardb.R;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterPhoneActivity extends AppCompatActivity {
 
@@ -11,5 +17,20 @@ public class RegisterPhoneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_phone);
+
+        EditText editTextPhoneNumber = findViewById(R.id.edt_register_phone);
+        editTextPhoneNumber.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+        findViewById(R.id.btn_register_phone).setOnClickListener(view -> {
+            editTextPhoneNumber.setText("");
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Numero cadastrado com sucesso");
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            builder.show();
+        });
     }
 }
